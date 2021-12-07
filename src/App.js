419496a2daf1be7500816ec16febfde5ac558240
Emerson Oliveira <emerson.contato@gmail.com>
2021-12-07 15:48:48 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
+import Table from './components/Table';
+import StartWarsProvider from './context/StartWarsProvider';
 
 function App() {
   return (
-    <span>Hello, App!</span>
+    <StartWarsProvider>
+      <span>Hello, App!</span>
+      <Table />
+    </StartWarsProvider>
   );
 }
 
---
+++ src/context/StartWarsProvider.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import StartWarsContext from './StartWarsContext';
+
+function StartWarsProvider({ children }) {
+  const [data, setData] = useState([]);
+
+  const fetchStartWars = async () => {
+    const request = await fetch('https://swapi-trybe.herokuapp.com/api/planets/');
+    const resquestJson = await request.json();
+    const { results } = resquestJson;
+    setData(results);
+  };
+
+  const contextValue = {
+    data,
+  };
+
+  useEffect(() => {
+    fetchStartWars();
+  }, []);
+
+  return (
+    <StartWarsContext.Provider value={ contextValue }>
+      { children }
+    </StartWarsContext.Provider>
+  );
+}
+
+StartWarsProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default StartWarsProvider;
